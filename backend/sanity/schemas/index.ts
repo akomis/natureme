0c7f32a7b12f4a05671118a3edffd1b85260e68b
akomis <akomis@protonmail.com>
2024-04-02 13:36:12 +0300

    removed product and category from sanity as it will be fetched from medusa

--- backend/sanity/schemas/index.ts
+++ backend/sanity/schemas/index.ts
@@ -1,14 +1,6 @@
 import Global from './global'
 import Activity from './activity'
-import Product, { Category } from './product'
 import Testimonial from './testimonial'
 import FAQ from './faq'
 
-export const schemaTypes = [
-  Global,
-  Activity,
-  Product,
-  Category,
-  Testimonial,
-  FAQ,
-]
+export const schemaTypes = [Global, Activity, Testimonial, FAQ]
