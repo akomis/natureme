85a82033b1490ed147d13657562cbb1556d2282d
akomis <akomis@protonmail.com>
2024-04-20 14:35:25 +0300

    polishing on products list

--- frontend/src/app/catalogue/page.tsx
+++ frontend/src/app/catalogue/page.tsx
@@ -18,27 +18,15 @@ export default function Catalogue() {
     );
   }
 
-  const productsWithVariants = products.filter(
-    ({ variants }) => variants.length > 1
-  );
-  const productsWithoutVariants = products.filter(
-    ({ variants }) => variants.length === 1
-  );
-
   return (
-    <Screen className="relative items-start min-w-[70vw] pt-20 pb-10">
-      <div className="fixed top-0 flex h-auto min-w-[70vw] left-1/2 transform -translate-x-1/2 justify-between mt-20 z-10 gap-10">
+    <Screen className="relative items-start max-w-[80vw] pt-20 pb-10">
+      <div className="flex h-auto justify-between mt-20 z-10 gap-20">
         <PageHeader title={"Catalogue"} />
         <Cart items={[]} />
       </div>
-      <div className="flex flex-col gap-10 overflow-scroll mt-20 rounded-lg">
-        <div className="w-full flex flex-col gap-10">
-          {productsWithVariants.map(({ title, variants }: any) => (
-            <ProductList key={title} header={title} variants={variants} />
-          ))}
-        </div>
-        <div className="w-full flex flex-wrap gap-5">
-          {productsWithoutVariants.map(({ title, variants }: any) => (
+      <div className="flex flex-col gap-10 w-full overflow-scroll mt-10 rounded-lg">
+        <div className="flex flex-wrap gap-10">
+          {products.map(({ title, variants }: any) => (
             <ProductList key={title} header={title} variants={variants} />
           ))}
         </div>
