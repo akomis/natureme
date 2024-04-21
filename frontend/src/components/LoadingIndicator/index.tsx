f6b7e8a83a089f4fa5cc0b37ae30f5ee5f89ba99
akomis <akomis@protonmail.com>
2024-04-21 13:45:53 +0300

    fix document error

--- frontend/src/components/LoadingIndicator/index.tsx
+++ frontend/src/components/LoadingIndicator/index.tsx
@@ -1,5 +1,8 @@
-import Lottie from "lottie-react";
+"use client";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 import animation from "./animation.json";
+import dynamic from "next/dynamic";
 
 const LoadingIndicator = () => {
   return <Lottie animationData={animation} loop={true} />;
