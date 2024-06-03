097b256a80ffa7b7f0c27a1a1962e78055ba3ee9
akomis <akomis@protonmail.com>
2024-06-04 00:50:46 +0300

    fix next js build error because of missing suspense

--- frontend/src/app/confirmation/layout.tsx
+++ frontend/src/app/confirmation/layout.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import LoadingIndicator from "@/components/LoadingIndicator";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { QueryClient } from "@tanstack/react-query";
 import { MedusaProvider } from "medusa-react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY ?? "pk_");
 const queryClient = new QueryClient();
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const StripeProvider = ({ children }: { children: React.ReactNode }) => {
   const searchParams = useSearchParams();
 
   const clientSecret = searchParams.get("payment_intent_client_secret") ?? "";
@@ -17,12 +19,24 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <Elements stripe={stripePromise} options={{ clientSecret, loader }}>
-      <MedusaProvider
-        queryClientProviderProps={{ client: queryClient }}
-        baseUrl={process.env.NEXT_PUBLIC_MEDUSA_URL ?? "http://localhost:8000"}
-      >
-        {children}
-      </MedusaProvider>
+      {children}
     </Elements>
+  );
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Suspense fallback={<LoadingIndicator />}>
+      <StripeProvider>
+        <MedusaProvider
+          queryClientProviderProps={{ client: queryClient }}
+          baseUrl={
+            process.env.NEXT_PUBLIC_MEDUSA_URL ?? "http://localhost:8000"
+          }
+        >
+          {children}
+        </MedusaProvider>
+      </StripeProvider>
+    </Suspense>
   );
 }
