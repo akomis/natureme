1af03766c39d6f713de0e1c9a6b7eb977b565eb7
akomis <akomis@protonmail.com>
2024-07-05 16:51:59 +0300

    fix typo

--- backend/medusa/src/loaders/notification.ts
+++ backend/medusa/src/loaders/notification.ts
@@ -3,12 +3,13 @@ import { Logger, MedusaContainer, NotificationService } from "@medusajs/medusa";
 export default async (container: MedusaContainer): Promise<void> => {
   const logger = container.resolve<Logger>("logger");
 
-  logger.info("Starting loader...");
+  logger.info("Loading notifications service...");
   const notificationService = container.resolve<NotificationService>(
     "notificationService"
   );
 
   notificationService.subscribe("order.placed", "email-sender");
+  notificationService.subscribe("order.fullfillment_created", "email-sender");
   notificationService.subscribe("order.shipment_created", "email-sender");
   notificationService.subscribe("order.refund_created", "email-sender");
 };
