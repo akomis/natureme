417b307a7bde13e3865985beac4b6e28848189fe
akomis <akomis@protonmail.com>
2024-07-10 22:23:58 +0300

    comment out testimonials

--- frontend/src/app/_sections/Testimonials/index.tsx
+++ frontend/src/app/_sections/Testimonials/index.tsx
@@ -1,48 +1,48 @@
-import { fetchFromSanity, getRandomPastelColor } from "@/utils";
+// import { fetchFromSanity, getRandomPastelColor } from "@/utils";
 
-type ReviewCardProps = {
-  author: string;
-  review: string;
-  date: string;
-};
+// type ReviewCardProps = {
+//   author: string;
+//   review: string;
+//   date: string;
+// };
 
-const ReviewCard = ({ author, review, date }: ReviewCardProps) => {
-  const backgroundColor = getRandomPastelColor();
+// const ReviewCard = ({ author, review, date }: ReviewCardProps) => {
+//   const backgroundColor = getRandomPastelColor();
 
-  return (
-    <div
-      style={{ backgroundColor }}
-      className={
-        "card bg-base-300 opacity-1 px-4 py-2 h-fit w-3/5 min-w-[300px] max-w-[30%] hover:scale-110 hover:z-10 hover:shadow-md transition-all duration-300"
-      }
-    >
-      <a href="#">
-        <p className="text-2xl">{author}</p>
-        <p>{review}</p>
-        <p className="text-right">{date}</p>
-      </a>
-    </div>
-  );
-};
+//   return (
+//     <div
+//       style={{ backgroundColor }}
+//       className={
+//         "card bg-base-300 opacity-1 px-4 py-2 h-fit w-3/5 min-w-[300px] max-w-[30%] hover:scale-110 hover:z-10 hover:shadow-md transition-all duration-300"
+//       }
+//     >
+//       <a href="#">
+//         <p className="text-2xl">{author}</p>
+//         <p>{review}</p>
+//         <p className="text-right">{date}</p>
+//       </a>
+//     </div>
+//   );
+// };
 
-const Testimonials = async () => {
-  const testimonials = await fetchFromSanity("testimonial");
+// const Testimonials = async () => {
+//   const testimonials = await fetchFromSanity("testimonial");
 
-  return (
-    <section className="flex flex-col gap-4 h-fit justify-center items-center w-fit p-10">
-      <h2 className="text-4xl font-bold">Testimonials</h2>
-      <div className="flex flex-wrap justify-center items-center gap-4 p-4 max-h-[80vh] overflow-scroll">
-        {testimonials.map((testimonial) => (
-          <ReviewCard
-            key={testimonial.author}
-            author={testimonial.author}
-            review={testimonial.text}
-            date={testimonial.date}
-          />
-        ))}
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section className="flex flex-col gap-4 h-fit justify-center items-center w-fit p-10">
+//       <h2 className="text-4xl font-bold">Testimonials</h2>
+//       <div className="flex flex-wrap justify-center items-center gap-4 p-4 max-h-[80vh] overflow-scroll">
+//         {testimonials.map((testimonial) => (
+//           <ReviewCard
+//             key={testimonial.author}
+//             author={testimonial.author}
+//             review={testimonial.text}
+//             date={testimonial.date}
+//           />
+//         ))}
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Testimonials;
+// export default Testimonials;
