90f0f07981ed51d64d1696bc6c686b28b3bddb41
akomis <akomis@protonmail.com>
2024-10-10 22:23:59 +0300

    troubleshooting framer-motion

--- frontend/src/app/template.tsx
+++ frontend/src/app/template.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { Bounce, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -28,33 +28,32 @@ export default function Template({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   return (
-    <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
-      <motion.div
-        key={pathname}
-        variants={variants}
-        animate="in"
-        initial="out"
-        exit="out"
-        transition={{ ease: "easeInOut", duration: 1 }}
-      >
-        <BackgroundFlowers />
-        <div>{children}</div>
+    <motion.div
+      key={pathname}
+      variants={variants}
+      animate="in"
+      initial="out"
+      exit="out"
+      transition={{ ease: "easeInOut", duration: 1 }}
+      className="bg-jasmine"
+    >
+      <BackgroundFlowers />
+      <div>{children}</div>
 
-        <ToastContainer
-          position="bottom-center"
-          autoClose={3500}
-          hideProgressBar={true}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable={false}
-          pauseOnHover
-          theme="colored"
-          transition={Bounce}
-          limit={1}
-        />
-      </motion.div>
-    </AnimatePresence>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3500}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme="colored"
+        transition={Bounce}
+        limit={1}
+      />
+    </motion.div>
   );
 }
