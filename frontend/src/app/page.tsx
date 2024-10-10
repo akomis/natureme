8674f8ad5bc754d45f072b5a4c24c9d681fa1c70
akomis <akomis@protonmail.com>
2024-10-10 20:47:14 +0300

    troubleshooting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,14 +11,15 @@ export default function Index() {
       <About />
       <Footer />
 
-      <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2">
-        <Link href="/catalogue">
-          <button className="group hover:text-[#8D39AD] transition-all hover:cursor-pointer flex items-center gap-2 bg-primary hover:bg-secondary duration-300 px-4 py-2 rounded-2xl hover:z-10 hover:shadow-lg hover:scale-110">
-            <p className="font-bold text-2xl">Catalogue</p>
-            <ArrowRightCircle className="font-bold text-2xl group-hover:scale-125 transition-all duration-700" />
-          </button>
-        </Link>
-      </div>
+      <Link
+        href="/catalogue"
+        className="fixed bottom-10 left-1/2 transform -translate-x-1/2"
+      >
+        <button className="group hover:text-[#8D39AD] transition-all hover:cursor-pointer flex items-center gap-2 bg-primary hover:bg-secondary duration-300 px-4 py-2 rounded-2xl hover:z-10 hover:shadow-lg hover:scale-110">
+          <p className="font-bold text-2xl">Catalogue</p>
+          <ArrowRightCircle className="font-bold text-2xl group-hover:scale-125 transition-all duration-700" />
+        </button>
+      </Link>
     </div>
   );
 }
