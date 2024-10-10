827e69e889bc90f5ac329532b17c43b18a708241
akomis <akomis@protonmail.com>
2024-10-10 22:02:00 +0300

    investigation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,10 +11,7 @@ export default function Index() {
       <About />
       <Footer />
 
-      <Link
-        href="/catalogue"
-        className="fixed bottom-10 left-1/2 transform -translate-x-1/2"
-      >
+      <Link href="/catalogue" className="fixed bottom-10">
         <button className="group hover:text-[#8D39AD] transition-all hover:cursor-pointer flex items-center gap-2 bg-primary hover:bg-secondary duration-300 px-4 py-2 rounded-2xl hover:z-10 hover:shadow-lg hover:scale-110">
           <p className="font-bold text-2xl">Catalogue</p>
           <ArrowRightCircle className="font-bold text-2xl group-hover:scale-125 transition-all duration-700" />
